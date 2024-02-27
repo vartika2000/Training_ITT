6827f2ca07ccd0b8f8287e50762b672367e4feb6
Vartika Jain <vartika.jain@intimetec.com>
2024-02-27 09:16:44 +0530

    add the notes of the python

--- week_1/assignment1.ps1
+++
@@ -1,7 +0,0 @@
-function show{
-    param( $str )
-    Set-ExecutionPolicy -ExecutionPolicy $str 
-}
-$str = "Unrestricted"
-show( $str )
-Write-Host $str
--- week_4/assignment1.ps1
+++
@@ -1,8 +0,0 @@
-function show{
-   
-    param( $str1 )
-    Set-ExecutionPolicy -ExecutionPolicy $str 
-
-}
-$str = "Unrestricted"
-show( $str )
